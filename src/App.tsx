2561e26a80d44333a5aa159c424b9ae10292da49
Brad Zacher <brad.zacher@gmail.com>
2018-11-29 09:51:32 -0800

    Added tweaked create-react-app

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react'
+
+const App = React.memo(() => <div>APP</div>)
+
+export default App
---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+
+import App from '~/App'
+
+// this is the entrypoint for local development
+ReactDOM.render(<App />, document.getElementById('root') as HTMLElement)
